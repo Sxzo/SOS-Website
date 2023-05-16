22dd5d43ae3b9480311683f00e0f4b625c326a7a
Lev Sosani <sosanilev10@gmail.com>
2023-05-16 02:31:26 -0500

    Mobile Update

--- app.js
+++ app.js
@@ -1,45 +1,70 @@
-const opacityObserver = new IntersectionObserver((entries) => {
-    entries.forEach((entry) => {
-        console.log(entry)
-        if (entry.isIntersecting) {
-            entry.target.classList.add('showo')
-        } else {
-            entry.target.classList.remove('showo')
-        }
+function isMobileDevice() {
+    var screenWidth = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
+    var screenHeight = window.innerHeight || document.documentElement.clientHeight || document.body.clientHeight;
+  
+    return screenWidth < 768 || screenHeight < 768; // Adjust the threshold as per your requirements
+}
+  
+  // Usage
+if (isMobileDevice()) {
+    console.log("User is on a mobile device");
+    const hiddenOpacityElements = document.querySelectorAll(".hiddeno")
+    hiddenOpacityElements.forEach((el) => el.classList.remove("hiddeno"))
+
+    const hiddenSlideElements = document.querySelectorAll(".hiddens")
+    hiddenSlideElements.forEach((el) => el.classList.remove("hiddens"))
+
+    const hiddenSlideListElements = document.querySelectorAll(".hiddenlist")
+    hiddenSlideListElements.forEach((el) => el.classList.remove("hiddenlist"))
+} else {
+    
+}
+
+console.log("User is not on a mobile device");
+    const opacityObserver = new IntersectionObserver((entries) => {
+        entries.forEach((entry) => {
+            // console.log(entry)
+            if (entry.isIntersecting) {
+                entry.target.classList.add('showo')
+            } else {
+                entry.target.classList.remove('showo')
+            }
+        })
     })
-})
-
-
-const hiddenOpacityElements = document.querySelectorAll(".hiddeno")
-hiddenOpacityElements.forEach((el) => opacityObserver.observe(el))
-
-
-const slideObserver = new IntersectionObserver((entries) => {
-    entries.forEach((entry) => {
-        console.log(entry)
-        if (entry.isIntersecting) {
-            entry.target.classList.add('shows')
-        } else {
-            entry.target.classList.remove('shows')
-        }
+    
+    
+    const hiddenOpacityElements = document.querySelectorAll(".hiddeno")
+    hiddenOpacityElements.forEach((el) => opacityObserver.observe(el))
+    
+    
+    const slideObserver = new IntersectionObserver((entries) => {
+        entries.forEach((entry) => {
+            // console.log(entry)
+            if (entry.isIntersecting) {
+                entry.target.classList.add('shows')
+            } else {
+                entry.target.classList.remove('shows')
+            }
+        })
     })
-})
-
-
-const hiddenSlideElements = document.querySelectorAll(".hiddens")
-hiddenSlideElements.forEach((el) => slideObserver.observe(el))
-
-const slideListObserver = new IntersectionObserver((entries) => {
-    entries.forEach((entry) => {
-        console.log(entry)
-        if (entry.isIntersecting) {
-            entry.target.classList.add('showlist')
-        } else {
-            entry.target.classList.remove('showlist')
-        }
+    
+    
+    const hiddenSlideElements = document.querySelectorAll(".hiddens")
+    hiddenSlideElements.forEach((el) => slideObserver.observe(el))
+    
+    const slideListObserver = new IntersectionObserver((entries) => {
+        entries.forEach((entry) => {
+            // console.log(entry)
+            if (entry.isIntersecting) {
+                entry.target.classList.add('showlist')
+            } else {
+                entry.target.classList.remove('showlist')
+            }
+        })
     })
-})
+    
+    
+    const hiddenSlideListElements = document.querySelectorAll(".hiddenlist")
+    hiddenSlideListElements.forEach((el) => slideListObserver.observe(el))
 
 
-const hiddenSlideListElements = document.querySelectorAll(".hiddenlist")
-hiddenSlideListElements.forEach((el) => slideListObserver.observe(el))
