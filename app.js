c7be3a298248d2a2a0fbcbb9a0f4e4b8878924ed
Lev Sosani <sosanilev10@gmail.com>
2023-05-19 02:01:32 -0500

    Mobile site lag fix!!

--- app.js
+++ app.js
@@ -4,21 +4,6 @@ function isMobileDevice() {
   
     return screenWidth < 768 || screenHeight < 768; // Adjust the threshold as per your requirements
 }
-  
-  // Usage
-if (isMobileDevice()) {
-    console.log("User is on a mobile device");
-    const hiddenOpacityElements = document.querySelectorAll(".hiddeno")
-    hiddenOpacityElements.forEach((el) => el.classList.remove("hiddeno"))
-
-    const hiddenSlideElements = document.querySelectorAll(".hiddens")
-    hiddenSlideElements.forEach((el) => el.classList.remove("hiddens"))
-
-    const hiddenSlideListElements = document.querySelectorAll(".hiddenlist")
-    hiddenSlideListElements.forEach((el) => el.classList.remove("hiddenlist"))
-} else {
-    console.log("User is not on a mobile device");
-}
 
 
     const opacityObserver = new IntersectionObserver((entries) => {
